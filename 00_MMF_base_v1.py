b961582b8d0a5ce14755a14ba2857071175e4e30
beedlethabar <kleins.toby@awatapu.school.nz>
2021-07-07 13:29:14 +1200

    Base code

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,25 @@
+# Import statements
+
+
+# Functions go here
+
+
+# ********** Main Routine **********
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask user if they have used the program before and show
+
+# Loop to get ticket details
+
+# Get name(can't be blank)
+
+# Get age(between 12 and 130)
+
+# Calculate ticket price
+
+# Loop to ask for snacks
+
+# Calculate snack price
+
+# Ask for payment method (and apply surcharge if needed)
